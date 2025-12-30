f08410123c72e9a680e310572e6370154d4fa6f8
Amir Valie <amirvalie29@semnan.ac.ir>
2025-12-30 15:37:43 +0330

    Update repository owner and name to lowercase

--- pg-limiter.sh
+++ pg-limiter.sh
@@ -5,12 +5,12 @@ set -e
 # https://github.com/MatinDehghanian/PG-Limiter
 
 # Configuration
-REPO_OWNER="AmirValie2"
-REPO_NAME="TEST"
+REPO_OWNER="amirvalie2"
+REPO_NAME="test"
 SERVICE_NAME="pg-limiter"
 CONFIG_DIR="/etc/opt/pg-limiter"
 DATA_DIR="/var/lib/pg-limiter"
-DOCKER_IMAGE="ghcr.io/AmirValie2/test:latest"
+DOCKER_IMAGE="ghcr.io/amirvalie2/test:latest"
 COMPOSE_FILE="$CONFIG_DIR/docker-compose.yml"
 ENV_FILE="$CONFIG_DIR/.env"
 SCRIPT_URL="https://raw.githubusercontent.com/$REPO_OWNER/$REPO_NAME/main/pg-limiter.sh"
